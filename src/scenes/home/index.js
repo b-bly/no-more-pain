7584e19b3a9c8010dd77842a15e10b33af11fbc7
Brendt Bly <brendt.bly@outlook.com>
2018-02-07 15:37:01 -0600

    login form, navbar, home components added, routing added to App.js

---
+++ src/scenes/home/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Home extends Component {
+    render() {
+        return (
+            <div>
+                <span>No More Pain</span>
+            </div>
+        )
+
+    }
+}
+
+export default Home
