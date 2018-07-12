77c804fc3e4a01a185795ce1d1f9673a71e551ce
Brendt Bly <brendt.bly@outlook.com>
2018-07-12 15:09:07 -0500

    Testing stuff added

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import { mount } from "enzyme";
+import NavBar from "./scenes/navbar";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+// resource
+// https://medium.freecodecamp.org/the-right-way-to-test-react-components-548a4736ab22
+describe("NavBar", () => {
+  let props;
+  let mountedNavBar;
+  const navBar = () => {
+    if (!mountedNavBar) {
+      mountedNavBar = mount(
+        <NavBar {...props} />
+      );
+    }
+    return mountedNavBar;
+  }
+
+  beforeEach(() => {
+    props = {
+      loggedIn: undefined,
+
+    };
+    mountedNavBar = undefined;
+  });
+  
+  // Tests
+  describe("the rendered div", () => {
+    it("contains everything else that gets rendered", () => {
+      const divs = NavBar().find("div");
+      const wrappingDiv = divs.first();
+      expect(wrappingDiv.children()).toEqual(navBar().children());
+    });
+  });
 });
